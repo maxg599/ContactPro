076c6eaa07c12c0ec25f8d1752565b8c954c0f77
maxg599 <109822431+maxg599@users.noreply.github.com>
2022-11-01 19:08:27 -0400

    custom errors

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -10,6 +10,7 @@ using ContactPro.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using ContactPro.Models.ViewModels;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace ContactPro.Controllers
 {
@@ -17,17 +18,22 @@ namespace ContactPro.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IEmailSender _emailService;
 
-        public CategoriesController(ApplicationDbContext context, UserManager<AppUser> userManager)
+        public CategoriesController(ApplicationDbContext context, UserManager<AppUser> userManager, IEmailSender emailService)
         {
             _context = context;
             _userManager = userManager;
+            _emailService = emailService;
+
         }
 
         // GET: Categories
         [Authorize]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string swalMessage = null)
         {
+            ViewData["SwalMessage"] = swalMessage;
+
             string appUserId = _userManager.GetUserId(User);
 
 
@@ -61,25 +67,29 @@ namespace ContactPro.Controllers
             return View(model);
         }
 
+        //Email Categories Post//
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EmailCategory(EmailCategoryViewModel ecvm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _emailService.SendEmailAsync(ecvm.EmailData.EmailAddress, ecvm.EmailData.Subject, ecvm.EmailData.Body);
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Success: Email Sent!" });
+                }
+                catch
+                {
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Error: Email Did Not Send!" });
+                    throw;
+                }
+            }
+            return View(ecvm);
+        }
         // GET: Categories/Details/5
         [Authorize]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+        
 
         // GET: Categories/Create
         [Authorize]
@@ -181,10 +191,11 @@ namespace ContactPro.Controllers
             {
                 return NotFound();
             }
+            string appUserId = _userManager.GetUserId(User);
 
             var category = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                              .FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
             if (category == null)
             {
                 return NotFound();
@@ -198,17 +209,18 @@ namespace ContactPro.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Categories == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
-            }
-            var category = await _context.Categories.FindAsync(id);
+
+            string appUserId = _userManager.GetUserId(User);
+
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
             if (category != null)
             {
                 _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
         }
 
--- ContactPro/Controllers/HomeController.cs
+++ ContactPro/Controllers/HomeController.cs
@@ -23,6 +23,25 @@ namespace ContactPro.Controllers
             return View();
         }
 
+        [Route("/Home/HandleError/{code:int}")]
+        public IActionResult HandleError(int code)
+        {
+            var customError = new CustomError();
+
+            customError.code = code;
+
+            if(code == 404)
+            {
+                customError.message = "The Page is Not Found Right Meow";
+            }
+            else
+            {
+                customError.message = "Sorry, Something Went Wrong";
+            }
+
+            return View("~/Views/Shared/CustomError.cshtml", customError);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
