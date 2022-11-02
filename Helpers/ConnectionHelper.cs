56b0f6d235ac4135d804579e6e2823c02dd2e00d
maxg599 <109822431+maxg599@users.noreply.github.com>
2022-11-02 17:07:05 -0400

    again

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -9,7 +9,7 @@ namespace ContactPro.Helpers
             var connectionString = configuration.GetSection("pgSettings")["pgConnection"];
             var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-            return String.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
         }
         //build connection string from the environment for heroku//
         public static string BuildConnectionString(string databaseUrl)
