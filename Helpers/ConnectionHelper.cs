6f3479321455e9d1af817ad7d6fdb527db5773d2
maxg599 <109822431+maxg599@users.noreply.github.com>
2022-11-02 17:20:49 -0400

    i sware this the last time

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -15,7 +15,7 @@ namespace ContactPro.Helpers
         public static string BuildConnectionString(string databaseUrl)
         {
             var databaseUri = new Uri(databaseUrl);
-            var userInfo = databaseUri.UserInfo.Split(';');
+            var userInfo = databaseUri.UserInfo.Split(':');
             var builder = new NpgsqlConnectionStringBuilder
             {
                 Host = databaseUri.Host,
